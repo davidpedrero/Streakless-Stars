28f1e639c7fa260408a1552d27cc144dfbb29c56
David Pedrero <pedrero888@gmail.com>
2023-07-17 23:25:56 -0400

    layout bg color edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-blue-300`}>
+      <body className={`${inter.className} bg-blue-200`}>
         <div className='min-h-screen relative'>
           <Navbar />
           <main>
