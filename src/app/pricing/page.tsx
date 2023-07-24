5ea276fa62c6bb2149c18c977490e319a63a47de
David Pedrero <pedrero888@gmail.com>
2023-07-24 18:36:57 -0400

    completed pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+import doubleHung from '../../../public/double-hung.png';
+import halfMultiPane from '../../../public/half-multi-pane.png';
+import multiPane from '../../../public/multi-pane.png';
+import casement from '../../../public/casement.png';
+import twoPanelCasement from '../../../public/two-panel-casement.png';
+import threePanelCasement from '../../../public/three-panel-casement.png';
+
+
 export default function Pricing() {
   return (
     <>
@@ -21,10 +30,66 @@ export default function Pricing() {
         <div className='flex align-middle justify-center text-center'>
           <div className='flex justify-center'>
             <div className='bg-slate-900 grid grid-cols-2 text-fuchsia-50 text-4xl max-sm:text-xs max-md:text-sm max-lg:text-base'>
-              <div className='border-solid border-2 p-16'>Cost</div>
-              <div className='border-solid border-2 p-16'>Quality</div>
-              <div className='border-solid border-2 p-16'>Safety</div>
-              <div className='border-solid border-2 p-16'>Experience</div>
+              <div className='border-solid border-2 p-16'>
+                <Image className="mr-auto ml-auto mb-8"
+                  src={doubleHung}
+                  alt="Double Hung Window Pic"
+                  width={150}
+                  height={150}
+                />
+                <h4 className="mb-1 text-3xl">Double-Hung Window</h4>
+                <small className="text-slate-500 text-2xl">$15 - $25</small>
+              </div>
+              <div className='border-solid border-2 p-16'>
+                <Image className="mr-auto ml-auto mb-8"
+                  src={halfMultiPane}
+                  alt="Half Multi-Pane Window Pic"
+                  width={150}
+                  height={150}
+                />
+                <h4 className="mb-1 text-3xl">Half Multi-Pane Window</h4>
+                <small className="text-slate-500 text-2xl">$18 - $27</small>
+              </div>
+              <div className='border-solid border-2 p-16'>
+                <Image className="mr-auto ml-auto mb-8"
+                  src={multiPane}
+                  alt="Doublt Hung Window Pic"
+                  width={150}
+                  height={150}
+                />
+                <h4 className="mb-1 text-3xl">Multi-Pane Window</h4>
+                <small className="text-slate-500 text-2xl">$22 - $32</small>
+              </div>
+              <div className='border-solid border-2 p-16'>
+                <Image className="mr-auto ml-auto mb-8"
+                  src={casement}
+                  alt="Casement Window Pic"
+                  width={150}
+                  height={150}
+                />
+                <h4 className="mb-1 text-3xl">Casement Window</h4>
+                <small className="text-slate-500 text-2xl">$10 - $20</small>
+              </div>
+              <div className='border-solid border-2 p-16'>
+                <Image className="mr-auto ml-auto mb-8"
+                  src={twoPanelCasement}
+                  alt="2-Panel Casement Window Pic"
+                  width={150}
+                  height={150}
+                />
+                <h4 className="mb-1 text-3xl">2-Panel Casement Window</h4>
+                <small className="text-slate-500 text-2xl">$20 - $35</small>
+              </div>
+              <div className='border-solid border-2 p-16'>
+                <Image className="mr-auto ml-auto mb-8"
+                  src={threePanelCasement}
+                  alt="3-Panel Casement Window Pic"
+                  width={150}
+                  height={150}
+                />
+                <h4 className="mb-1 text-3xl">3-Panel Casement Window</h4>
+                <small className="text-slate-500 text-2xl">$30 - $45</small>
+              </div>
             </div>
           </div>
         </div>
