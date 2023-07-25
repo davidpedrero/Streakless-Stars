d38498b23b5cd254ed7ee60870d0353c33b88c8d
David Pedrero <pedrero888@gmail.com>
2023-07-25 16:34:50 -0400

    changed hero button route

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -2,39 +2,18 @@ export default function Book() {
   return (
     <>
       <div className='bg-blue-200 flex h-1/2 align-middle justify-center'>
-        <div className='text-center w-1/2'>
-          <h2 className='p-8 text-5xl'>About Us</h2>
+        <div className='text-center w-1/2 text-blue-950'>
+          <h2 className='p-8 text-5xl'>Book Appointment!</h2>
           <div className='text-left pb-8'>
-            <h2 className="text-2xl pb-1">
-              <strong>
-                NYC Window Cleaning - Steakless Stars
-              </strong>
-            </h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, veritatis? Recusandae fugit nobis natus dolores nisi molestiae, exercitationem, sint doloremque dolor inventore voluptatem fugiat sapiente dolore beatae cumque totam, asperiores distinctio obcaecati pariatur quam harum ipsum adipisci ab! Autem accusamus quam cumque. Accusamus quod iste atque hic beatae velit odio eligendi, perferendis vitae? Magni, eaque ad quibusdam, voluptatum nihil repellendus delectus doloremque aspernatur, soluta modi sit deleniti? Neque corrupti quaerat ipsum nemo nulla culpa asperiores, dignissimos exercitationem delectus in nam nisi, commodi repudiandae enim consequuntur optio mollitia dolor. Voluptas, fugiat labore quia perspiciatis cumque debitis similique atque! Eos reiciendis officiis quas voluptatibus aut voluptas placeat ratione inventore dignissimos odit aliquam suscipit sint laboriosam asperiores quibusdam, reprehenderit adipisci quo rerum accusantium tempore sequi nisi maiores magni ipsa. Ab adipisci corrupti illum iste labore, possimus soluta, molestias eum expedita voluptatibus minima fugit. Fuga blanditiis commodi corrupti quo in, perspiciatis veniam sit suscipit!</p>
-          </div>
-          <div className='text-left pb-8'>
-            <h2 className="text-2xl pb-1">
-              <strong>
-                Window Cleaning – Quality
-              </strong>
-            </h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum officiis illo quidem ea unde dolores nihil ratione pariatur asperiores dolorem impedit excepturi delectus sunt ad odio laborum obcaecati earum voluptatem perferendis, voluptatum nulla quam quo, dignissimos ducimus? Quidem, eaque sed maiores itaque unde ab fuga dolore, aliquam, sint facilis ratione porro inventore consectetur aspernatur eum ipsam minima nisi! Cumque soluta velit necessitatibus tenetur, eveniet quas libero, nisi quam expedita eos mollitia incidunt molestias ipsum optio laboriosam rem repellat eius inventore dolore temporibus! Iste, consequuntur cum? Harum, deserunt! Nemo, voluptatem cum perferendis sunt eveniet odio obcaecati deleniti fuga, esse atque enim incidunt unde dolores nihil? Aliquid illo placeat ducimus quisquam quasi. Quae, aspernatur a blanditiis eveniet explicabo modi fugiat neque tempora nostrum hic nemo aliquam fuga recusandae, ullam numquam dolore. Ad veniam reprehenderit, obcaecati perferendis quo voluptas facilis ducimus fuga amet id voluptates corporis, totam necessitatibus error fugit sit explicabo eius?</p>
-          </div>
-          <div className='text-left pb-8'>
-            <h2 className="text-2xl pb-1">
-              <strong>
-                Additional Services
-              </strong>
-            </h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo adipisci illum veritatis quae aperiam quasi sapiente error itaque esse nemo, officiis, aut dolor obcaecati tempore magnam mollitia laboriosam quos quo hic at placeat ullam aspernatur blanditiis? Ratione repellat deserunt corporis sit quidem adipisci dignissimos eius itaque excepturi saepe distinctio facere, ducimus fugit minus corrupti odit reprehenderit officia libero obcaecati, aut nam? Neque reiciendis, deserunt nostrum reprehenderit, voluptates iusto architecto tempora obcaecati soluta corporis minus voluptate earum aspernatur ut laborum, mollitia maxime ea error iste consequuntur? Dignissimos nostrum ipsam, vero commodi, ut sint aspernatur error inventore ad impedit fugit, repellendus nemo!</p>
-          </div>
-          <div className='text-left pb-8'>
-            <h2 className="text-2xl pb-1">
-              <strong>
-                Customer Service
-              </strong>
-            </h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus aliquam sapiente quaerat! Inventore, magni? Nisi, tempore sint. Repellendus omnis cumque voluptatibus natus dolore quidem voluptas modi ab quasi expedita, nesciunt voluptatum quisquam quo quis minima a voluptate. Soluta porro culpa sint quo deserunt nobis, vitae ea ipsa blanditiis saepe. Debitis, omnis officia! Asperiores illo, id nulla iusto eum deleniti eos ducimus quam optio cum? Eum, laborum nisi. In enim possimus reprehenderit! Expedita, quis nisi! Iure incidunt rem sequi fugiat nostrum non porro, quos pariatur excepturi. In, illo distinctio cumque ipsam dolor nesciunt. Assumenda impedit sapiente culpa modi non consequuntur temporibus.</p>
+            <form className="">
+              <label htmlFor="name">Your name</label>
+              <input id="name" className="mb-4" />
+              <br></br>
+              <label htmlFor="email">Your email</label>
+              <input id="enail" className="mb-4" />
+              <br></br>
+              <input type="submit" className="bg-blue-600 text-white p-2 rounded-md mt-4 cursor-pointer" />
+            </form>
           </div>
         </div>
       </div>
